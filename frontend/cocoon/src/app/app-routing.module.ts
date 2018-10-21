81f7f0420381835dd7905e0355c13b01fc9b0ff5
dhaveed <davidadegoke31@gmail.com>
2018-10-21 21:38:46 +0100

    settings view

--- frontend/cocoon/src/app/app-routing.module.ts
+++ frontend/cocoon/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'settings',
     pathMatch: 'full'
   },
   {
